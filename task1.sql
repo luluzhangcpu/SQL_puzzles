efec96a818952f6ee5f5556ec7a46b8a57410d53
Luluzhangcpu <42401339+yougebuding@users.noreply.github.com>
2022-04-13 15:05:11 +0800

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,1 +1,3 @@
-select a.id,a.price,b.sales from sp a left join product b on a.id = b.product_id;
+--第一题，将a表和b表 对应键连接起来，并取相对应的字段；
+select a.id,a.price,b.sales 
+from sp a left join product b on a.id = b.product_id;
